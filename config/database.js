d6c51cfac3f68fff72a1a4eb69fa44ef69b3872a
asepirfans <asepirfans.9c@gmail.com>
2023-05-21 12:42:43 +0700

    Test CI/CD 0.8

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 const db = new Sequelize(process.env.DB_DATABASE,process.env.DB_NAME,process.env.DB_PASSWORD,
     {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'mysql'
     }
 );
 
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   });
 app.use(router);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
 });
