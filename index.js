df52a18ab421ebc5e67ed2b061226df79a379bed
asepirfans <asepirfans.9c@gmail.com>
2023-05-25 05:59:25 +0700

    Test CI/CD 1.9

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-    res.send("API Ready");
+    res.send("API Ready ");
   });
 app.use(router);
 
